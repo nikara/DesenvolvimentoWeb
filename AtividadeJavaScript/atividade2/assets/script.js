6644bc20b62efe5aeb3092eb88dea6c67dd0b6b0
nikara <nickolaslamin@gmail.com>
2025-10-17 22:16:46 -0300

    Add HTML structure, CSS styles, and JavaScript functionality for cinema feedback form

---
+++ AtividadeJavaScript/atividade2/assets/script.js
@@ -0,0 +1,16 @@
+/*
+2.Cada espectador de um cinema respondeu a um questionário no qual constava sua idade e 
+a sua opinião em relação ao filme: ótimo - 3, bom - 2, regular -1. 
+Faça um programa que receba a idade e a opinião de 15 espectadores, calcule e imprima: 
+●  a média das idades das pessoas que responderam ótimo; 
+●  a quantidade de pessoas que responderam regular; 
+●  a porcentagem de pessoas que responderam bom entre todos os espectadores 
+analisados.
+*/
+
+// Criar um enum para 
+const opiniao = Object.freeze({
+    Otimo: 3,
+    Bom: 2,
+    regular: 1,
+});
