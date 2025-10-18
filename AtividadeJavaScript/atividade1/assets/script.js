364b44cf33832f44aa05ec2d792fd493502d2405
nikara <nickolaslamin@gmail.com>
2025-10-17 21:41:31 -0300

    Refactor data generation and display logic for height and gender statistics

--- AtividadeJavaScript/atividade1/assets/script.js
+++ AtividadeJavaScript/atividade1/assets/script.js
@@ -6,29 +6,71 @@ programa que calcule e escreva:
 ●  o número de homens.
 */
 
-// Gerador de homens e mulheres 
+//region Objeto pessoa
 
-const sexoArray = [];
+const pessoa = {
+    sexo:"",
+    altura: null
+};
+// Array de pessoas
+const pessoas = Array.from({length: 15}, () => ({...pessoa}));
+//#endregion
+
+//#region Gerador de dados
 for( let i = 0; i < 15; i++){
     const numeroBinario = Math.floor(Math.random()*2);
-    if(numeroBinario === 0){
-        sexoArray.push("M");
-    }else if(numeroBinario === 1){
-        sexoArray.push("F");
-        
+    pessoas[i].sexo = numeroBinario === 0 ? "M" : "F";
+    const altura = (Math.random() * (2.00 - 1.50) + 1.50);
+    pessoas[i].altura = parseFloat(altura.toFixed(2));
+}
+//#endregion
+
+//#region Loop para coletar os dados do exercicio
+let maior = pessoas[0].altura;
+let menor = pessoas[0].altura
+let media = 0;
+let numeroDeMulheres = 0;
+let numeroDeHomens = 0;
+
+for(let i = 0; i < pessoas.length; i++){
+    const alturaAtual = pessoas[i].altura;
+    if(alturaAtual > maior){
+        maior = alturaAtual;
+    }
+    if(alturaAtual < menor){
+        menor = alturaAtual;
+    }
+    
+    if(pessoas[i].sexo === "F"){
+        media = media + alturaAtual;
+        numeroDeMulheres++
     }
 
+    if(pessoas[i].sexo === "M"){
+        numeroDeHomens++;
+    }
 }
+//#endregion
 
-// Gerador de alturas 
+//#region Inserção dos dados
+const tabela = document.querySelector("#tabelaPessoa tbody");
+pessoas.forEach((pessoa,index) => {
+    const linha = document.createElement("tr");
+    linha.innerHTML =`
+        <td>${index+1}</td>
+        <td>${pessoa.sexo}</td>
+        <td>${pessoa.altura}</td>
+    `;
+    tabela.appendChild(linha);
+});
 
-const alturaArray = [];
-for (let i = 0; i < 15; i++){
-    const altura = (Math.random() * (2.00 - 1.50) + 1.50);
-    alturaArray.push(altura)
-}
-
-// Maior e Menor Alturas
+const dados = document.getElementById("resumoDados");
+resumo.innerHTML =`
+Maior altura: ${maior}m<br>
+Menor altura: ${menor}m<br>
+Média de altura das mulheres: ${media}m<br>
+Número de homens: ${numeroDeHomens}
+`;
 
 
 
