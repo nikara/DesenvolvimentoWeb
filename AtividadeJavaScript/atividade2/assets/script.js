63199599a7ce928b5dbfe7275d41d684c0cba7ed
nikara <nickolaslamin@gmail.com>
2025-10-20 17:50:26 -0300

    Implement cinema feedback form functionality and improve HTML structure

--- AtividadeJavaScript/atividade2/assets/script.js
+++ AtividadeJavaScript/atividade2/assets/script.js
@@ -12,5 +12,60 @@ analisados.
 const opiniao = Object.freeze({
     Otimo: 3,
     Bom: 2,
-    regular: 1,
+    Ruim: 1,
+});
+
+const resposta = [];
+const formulario = document.getElementById("formulario");
+const resultado = document.createElement("div");
+resultado.id = "resultado";
+document.body.appendChild(resultado);
+
+formulario.addEventListener("submit",function (event){
+    event.preventDefault();
+    const nome = document.getElementById("nome").value;
+    const idade = parseInt(document.getElementById("idade").value);
+    const opiniaoSelecionada = parseInt(document.querySelector('input[name="opiniao"]:checked')?.value);
+
+    if(!nome || isNaN(idade)|| isNaN(opiniaoSelecionada)){
+        alert("Preencha todos os campos corretamente.");
+        return;
+    }
+
+    resposta.push({nome,idade,opiniao});
+
+    // Limpa o formulário
+    formulario.reset();
+
+    if(resposta.length === 15){
+        let somaIdadeOtimo = 0;
+        let qtdOtimo = 0;
+        let qtdRuim = 0;
+        let qtdBom = 0;
+        
+        resposta.forEach(pessoa => {
+            if(pessoa.opiniao === opiniao.Otimo){
+                somaIdadeOtimo += pessoa.idade;
+                qtdOtimo++;
+            }
+            if(pessoa.opiniao === opiniao.Ruim){
+                qtdRuim++;
+            }
+            if(pessoa.opiniao === opiniao.Bom){
+                qtdBom++;
+            }
+        });
+
+        const mediaOtimo = qtdOtimo > 0 ? (somaIdadeOtimo / qtdOtimo).toFixed(2) : 0;
+        const porcentagemBom = ((qtdBom / resposta.length) * 100).toFixed(2);
+
+        resultado.innerHTML = `
+        <h3> Resultado da Pesquisa </h3>
+        <p>Média das idades de quem respondeu "Ótimo": ${mediaOtimo}</p>
+        <p>Média das idades de quem respondeu "Ruim": ${qtdRuim}</p>
+        <p>Porcentagem de pessoas que responderam "Bom": ${porcentagemBom}%</p>
+        `;
+    }else{
+        resultado.innerHTML = `<p>Resposta registradas: ${resposta.length}/15</p>`;
+    }
 });
