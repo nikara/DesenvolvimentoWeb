3a31ad753a86f68e6b727e7256065bacaeb11e67
nikara <nickolaslamin@gmail.com>
2025-10-20 18:35:31 -0300

    Add HTML structure, CSS styles, and JavaScript functionality for number input program

---
+++ AtividadeJavaScript/atividade3/assets/script.js
@@ -0,0 +1,8 @@
+/*
+ 3.Faça um programa que receba 10 números, calcule e imprima a soma dos números 
+pares e a soma dos números primos.
+*/
+
+const numero = document.getElementById("numero");
+
+const TabelaDeNumero = document.createElement("div")
