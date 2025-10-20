1f53036bf4eb1e7caf89ac83a08833ce6c3e2f32
nikara <nickolaslamin@gmail.com>
2025-10-20 19:42:57 -0300

    Implement number input program to calculate sums of even and prime numbers

--- AtividadeJavaScript/atividade3/assets/script.js
+++ AtividadeJavaScript/atividade3/assets/script.js
@@ -2,7 +2,48 @@
  3.Faça um programa que receba 10 números, calcule e imprima a soma dos números 
 pares e a soma dos números primos.
 */
+const formulario = document.getElementById("formulario");
+const lista = document.getElementById("listaNumeros");
+let cont = 0;
+let somaPares = 0;
+let somaPrimos = 0; 
 
-const numero = document.getElementById("numero");
+function DetectaPrimo(n){
+    if(n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++){
+        if(n % i === 0) return false;
+    }
+    return true;
+}
 
-const TabelaDeNumero = document.createElement("div")
+formulario.addEventListener("submit",function (event){
+    event.preventDefault();
+    const numero = document.getElementById("numero").value;
+    const valor = Number(numero);
+    
+
+    if(numero === "") return;
+    if(valor === 0)return;
+    if(cont >= 10) return;
+
+    if(valor%2 === 0){
+        somaPares += valor;
+    }
+    if(DetectaPrimo(valor)){
+        somaPrimos += valor;
+    }
+    
+    
+
+    cont++;
+    if(cont === 10){
+        const resultado = document.createElement("p");
+        resultado.textContent = `Soma dos Pares: ${somaPares} Soma dos Primos: ${somaPrimos}`;
+        document.body.appendChild(resultado);
+    }
+    const novoItem = document.createElement("li");
+    novoItem.textContent = numero;
+
+    lista.appendChild(novoItem);
+    document.getElementById("numero").value = "";
+});
